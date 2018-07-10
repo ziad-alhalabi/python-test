784a464f00fee875d16fd753db74cb658943e86d
ziad-alhalabi <35598793+ziad-alhalabi@users.noreply.github.com>
2018-07-10 18:25:27 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,46 +1,42 @@
-import json
-from pprint import pprint
-
 import os
 import pandas as pd
 import matplotlib.pyplot as plt
 
-df = pd.read_csv("weather_data.csv")
 
 newpath = r'results'
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
 
-print("Minimum Temperature Summary:")
-print(df['minTemp'].describe())
+# Data to plot
+labels = 'Camden', 'Paddington', 'Stratford', 'Greenwich'
+sizes = [215, 130, 245, 210]
+colors = ['gold', 'yellowgreen', 'lightcoral', 'lightskyblue']
+explode = (0.1, 0, 0, 0)  # explode 1st slice
+
+# Plot
+plt.pie(sizes, explode=explode, labels=labels, colors=colors,
+        autopct='%1.1f%%', shadow=True, startangle=140)
+
+plt.axis('equal')
+plt.savefig('results/pollution.png')
 
 
 
-fig = plt.figure()
-ax = plt.subplot(111)
-ax.plot(df['time'], df['minTemp'])
-plt.xlabel("Date")
-plt.ylabel("Temperature")
-plt.title("Minimum Temperature per day for June")
+df = pd.DataFrame(pd.np.random.randint(10, 35, size=(7, 4)), columns=list('ABCD'))
+
+df = df.rename(columns={'A': 'London', 'B': 'Liverpool'})
+
+ax = df[['London','Liverpool']].plot(kind='bar', title ="Difference in Temperature", figsize=(15, 10), legend=True, fontsize=12)
+ax.set_xlabel("Date", fontsize=12)
+ax.set_ylabel("Temperature", fontsize=12)
+
+plt.savefig('results/temperature.png')
 
 
-fig.savefig('results/minTemperature.png')
+print("London Weather:")
+print(df['London'].describe())
 
 
-
-
-print("Maximum Temperature Summary:")
-print(df['maxTemp'].describe())
-
-
-
-fig = plt.figure()
-ax = plt.subplot(111)
-ax.plot(df['time'], df['maxTemp'])
-plt.xlabel("Date")
-plt.ylabel("Temperature")
-plt.title("Maximum Temperature per day for June")
-
-
-fig.savefig('results/maxTemperature.png')
+print("Liverpool Weather:")
+print(df['London'].describe())
